eb3cc5b434a109a9c86d3a29907edae72245a96c
NamachiS007 <namachissnv@gmail.com>
2025-04-20 20:18:47 +0530

    backend deployed link integrated

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -41,7 +41,8 @@ export default function ChatPage() {
     
     try {
       // Use absolute URL to Flask backend
-      const response = await fetch('http://localhost:5000/chat', {
+      // const response = await fetch('http://localhost:5000/chat', {
+      const response = await fetch('https://namachissnv.pythonanywhere.com/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
